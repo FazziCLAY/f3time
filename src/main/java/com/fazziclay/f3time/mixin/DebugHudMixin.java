97990225568e0285aa62011f29eae85e5330c1c1
fazziclay <fazziclay@gmail.com>
2023-04-19 20:22:28 +0300

    yesssss

--- src/main/java/com/fazziclay/f3time/mixin/DebugHudMixin.java
+++ src/main/java/com/fazziclay/f3time/mixin/DebugHudMixin.java
@@ -12,11 +12,8 @@ import java.util.List;
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
 	@Inject(at = @At("RETURN"), method = "getLeftText")
-	private List<String> getLeftText(CallbackInfoReturnable<List<String>> info) {
+	private void getLeftText(CallbackInfoReturnable<List<String>> info) {
 		List<String> r = info.getReturnValue();
-
 		r.add(F3Time.genText());
-
-		return r;
 	}
 }
